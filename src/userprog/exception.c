effcfe3c84b2c4ce0f42809c74b4191604757e82
Shir Aharon <saharon@caltech.edu>
2013-02-20 08:41:44 -0500

    Updated page handler to more correctly terminate user programs

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -4,6 +4,7 @@
 #include "userprog/gdt.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 
 /*! Number of page faults processed. */
 static long long page_fault_cnt;
@@ -133,15 +134,20 @@ static void page_fault(struct intr_frame *f) {
     not_present = (f->error_code & PF_P) == 0;
     write = (f->error_code & PF_W) != 0;
     user = (f->error_code & PF_U) != 0;
-
-    /* To implement virtual memory, delete the rest of the function
-       body, and replace it with code that brings in the page to
-       which fault_addr refers. */
-    printf("Page fault at %p: %s error %s page in %s context.\n",
-           fault_addr,
-           not_present ? "not present" : "rights violation",
-           write ? "writing" : "reading",
-           user ? "user" : "kernel");
-    kill(f);
+    
+    /* Determine if in system call and end user program */
+    if (!user && is_user_vaddr(fault_addr)) {
+        kill_current_thread(-1);
+    } else {
+        /* To implement virtual memory, delete the rest of the function
+           body, and replace it with code that brings in the page to
+           which fault_addr refers. */
+        printf("Page fault at %p: %s error %s page in %s context.\n",
+               fault_addr,
+               not_present ? "not present" : "rights violation",
+               write ? "writing" : "reading",
+               user ? "user" : "kernel");
+        kill(f);
+    }
 }
 
